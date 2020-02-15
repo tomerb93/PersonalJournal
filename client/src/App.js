b56df920cb20170d99d8dfaa093c273775054e52
Tomer <tomerb93@gmail.com>
2020-02-15 12:34:36 +0200

    npx client-react-app & minor preps

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <Fragment>
+            <h1>App</h1>
+        </Fragment>
+    );
+};
+
+export default App;
